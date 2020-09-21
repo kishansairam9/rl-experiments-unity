4e4e7bb1ed45909e1d31ce6ed5e0018578e699e2
Abhay <abhayraw1@github.com>
2020-09-21 18:01:17 +0530

    updates

---
+++ Assets/FixRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    float yaw;
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(90f, 0f, -transform.parent.eulerAngles.y);
+    }
+}
